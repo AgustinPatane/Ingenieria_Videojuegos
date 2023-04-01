a699aacc39d2afe7d4953c38c9add97d5481f7f9
francoercoli <francoercoli7@gmail.com>
2023-04-01 16:07:19 -0300

    botones menu

--- Trabajo_Final/Menu.gd
+++ Trabajo_Final/Menu.gd
@@ -1,21 +1,15 @@
 extends Control
 
+onready var musica = get_node("Musica_OnOff/Musica")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	musica.play()
 
 func _on_Jugar_pressed():
 	get_tree().change_scene("res://producto/assets/scenes/Mapa.tscn")
 
+func _on_CheckButton_toggled(button_pressed):
+	if (button_pressed):
+		musica.play()
+	else:
+		musica.stop()
