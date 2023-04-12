db3b549372dc4648b4f2ff349546961c620b8eca
Franco Ercoli <89115165+francoercoli@users.noreply.github.com>
2023-04-12 11:47:31 -0300

    Tienda 0.11

--- Trabajo_Final/producto/assets/scenes/Tienda.gd
+++ Trabajo_Final/producto/assets/scenes/Tienda.gd
@@ -1,10 +1,20 @@
 extends Control
 
-onready var puntos = get_node("Puntos")
-var puntos_jugador = 10000
+onready var label_puntos = get_node("Puntos")
+var puntos = 10000
 
 func _ready():
-	puntos.text = str(puntos_jugador)
+	label_puntos.text = str(puntos)
 
 func _process(delta):
 	pass
+
+func comprar_item(item):
+	if puntos >= item.costo:
+		puntos -= item.costo
+		var purchased_item = item.instance()
+		add_child(purchased_item)
+	else:
+		print("Not enough money!")
+
+
