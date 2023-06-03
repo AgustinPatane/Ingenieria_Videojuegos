8ec2fd502622aeaa196121e601c94b7f1320fc42
Nahuel Spatera <spateranahuel@gmail.com>
2023-06-03 11:29:00 -0300

    Parpadeo de capsula

--- Trabajo_Final/producto/assets/scenes/Control.gd
+++ Trabajo_Final/producto/assets/scenes/Control.gd
@@ -6,13 +6,17 @@ onready var capsula := get_node("/root/"+Engine.get_meta("nombre_escena_mapa")+"
 onready var indicador := $Indicador 
 var jugador
 
+var blinkSpeed = 1.0
+var isBlinking = true
+
 func _ready():
 	jugador = get_node("/root/"+Engine.get_meta("nombre_escena_mapa")+"/Jugador")
-
+	capsula.material.set_shader_param("luminosity_amount", 0.0)
 
 
 
 func _process(delta):
+	
 	if jugador.experiencia>=jugador.experiencia_necesaria:
 		indicador.visible=true
 		$Label.visible=true
@@ -23,7 +27,22 @@ func _process(delta):
 		direction.y-=620
 		var rotation = direction.angle()*180/3.14
 		indicador.rotation_degrees=rotation
+		parpadeo(delta)
 	else:
 		indicador.visible=false
 		$Label.visible=false
 		$Shift.visible=false
+		capsula.material.set_shader_param("luminosity_amount", 0)
+
+
+
+
+func parpadeo(delta):
+	blinkSpeed += delta * 2  # Ajusta la velocidad del parpadeo según tus preferencias
+	var blink = abs(sin(blinkSpeed))
+	capsula.material.set_shader_param("luminosity_amount", blink*0.7)
+
+
+
+
+	
